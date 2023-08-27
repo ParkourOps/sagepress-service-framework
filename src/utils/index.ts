97bb8c408f68f968b04955b8024e8ed6b84077d9
Tej Birring <tejbirring@gmail.com>
2023-08-27 06:24:08 +0100

    implement function to create deep readonly copies of objects.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,12 @@
 import {freeze} from "icepick";
 import {DeepReadonly} from "ts-essentials";
+import _ from "lodash";
 
 export function makeReadonly<T>(input: T) {
     return freeze(input) as DeepReadonly<T>;
+}
+
+export function makeReadonlyCopy<T>(input: T) {
+    const copy = _.cloneDeep(input);
+    return makeReadonly(copy);
 }
